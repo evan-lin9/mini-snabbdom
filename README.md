c31ae1ade5d961ff09eb6735c3285d158ea1b761
Evan Lin <evan.lin@binance.com>
2021-01-12 10:36:05 +0800

    feat: 上传流程图，补充reade.me

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # mini-snabbdom
+
+实现 sanbbdom 核心代码
+
+### 版本迭代
+- [x] 环境配置
+- [x] 绘制流程图：
+  - [x] patch 函数执行过程
+  - [x] diff 算法执行过程 
+  - [ ] updateChildren 执行过程
+- [x] 核心函数实现
+  - [ ] `vnode` 、`h` 、`createElement`
+  - [ ] `pacth`
+  - [ ] `updateChildren`
+- [ ] 输出文章
+
+
+
+![patch函数执行过程](http://qm6xd205h.hd-bkt.clouddn.com/WeChat7fa6fbdf38f01caa8c25842ec67c277f.png)
+
+
+
+![diff 算法流程](http://qm6xd205h.hd-bkt.clouddn.com/WeChatbfc3ff8281e83d4b0c89a9428534703d.png)
+
--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,21 @@ import { h } from './mini-snabbdom/h'
 import { patch } from './mini-snabbdom/patch'
 
 const root: HTMLElement = document.getElementById('root');
+const vnode1 = h('h1', '宇哥在学习')
 
-const vnode = h(
+const vnode2 = h(
   'ul', {}, [
     h('li', {}, 'A'),
     h('li', {}, 'B'),
-    h('li', {}, 'C'),
+    h('li', {}, [
+      h('h1', '标题'),
+      h('p', '内容')
+    ]),
   ]
 )
-console.log(vnode)
-patch(root, vnode)
+patch(root, vnode1)
+
+document.getElementById('btn').addEventListener('click', function() {
+  patch(vnode1, vnode2)
+})
 
